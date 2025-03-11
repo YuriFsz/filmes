980d7cfc9ab8979867d899e6fde0ad4f822490e0
Yuri Fernandes <fszyuri@gmail.com>
2025-03-10 23:20:23 -0300

    Post Project

---
+++ movies-app/src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import './index.scss';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1 className="filmes page-title">Filmes</h1>
+            <h1 className='series page-title'>Series</h1>
+        </nav>
+    )
+}
+
